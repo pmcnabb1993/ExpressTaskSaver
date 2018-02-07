d0c027c447007797e708840c21e61262e162c024
Preston McNabb <prestonmcnabb@Prestons-MacBook-Pro.local>
2018-02-06 21:01:48 -0600

    Built out server.js with correct functions to create and delete and plan. Created database and planner table within mysql pro

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE day_planner_db;
+USE day_planner_db;
+
+-- Create the table plans.
+CREATE TABLE plans
+(
+id int NOT NULL AUTO_INCREMENT,
+plan varchar(255) NOT NULL,
+PRIMARY KEY (id)
+);
+
+-- Insert a set of records.
+INSERT INTO plans (plan) VALUES ('Plan to fight a ninja.');
